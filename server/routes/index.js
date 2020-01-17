052023222368c6b8d0cae2d2c6010899eed33178
a.chepugov <oleksii.chepugov@ria.com>
2020-01-17 18:20:54 +0200

    ref (routes)

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,28 +1,18 @@
-const config = require('config');
-const express = require('express');
-const path = require('path');
-
-const react = require('./react');
+const config = require( 'config' );
+const path = require( 'path' );
 
 const outputDir = config.webpack.output;
-const publicWeb = config.webpack.public.web;
 
-const Cell = require('../../helpers/Cell');
+const assetsPaths = {
+	node: path.join( '../..', outputDir, 'node', 'assets.json' ),
+	web: path.join( '../..', outputDir, 'web', 'assets.json' )
+}
 
-const assetsPathNode = path.join('../..', outputDir, 'node', 'assets.json');
-const assetsPathWeb = path.join('../..', outputDir, 'web', 'assets.json');
-const assets = {
-	node: require(assetsPathNode),
-	web: require(assetsPathWeb),
-};
-
-const ssr = require(assets.node.main.js)
-
-module.exports = (app) => {
-	app.use(publicWeb, express.static(`./${outputDir}/web/`));
-
+module.exports = ( app ) => {
 	app
-		.get('/_', (request, response) => response.send(config.app))
+		.get( '/_', ( request, response ) => response.send( config.app ) )
 
-		react(app, new Cell(ssr), new Cell(assets))
+	process.env.NODE_ENV === 'development' ?
+		require( './webpack' )( app, assetsPaths ) :
+		require( './react' )( app, assetsPaths );
 };
