f38bc36ede50658bbc2cef349755bca3a5f8ec71
a.chepugov <oleksii.chepugov@ria.com>
2020-01-20 17:03:04 +0200

    chore () redundant files

--- webpack/rules1/cache.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = (env, outputPath) =>
-	({
-		loader: 'cache-loader',
-		options: {
-			cacheDirectory: path.resolve(outputPath, '.cache-loader')
-		}
-	});
